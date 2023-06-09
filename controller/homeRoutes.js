2c16e064df34b0fd636a4282687b0579fdee4722
David Girton <dgirton2@gmail.com>
2023-06-08 21:33:37 -0400

    updaterout

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,11 +1,26 @@
 const router = require('express').Router()
 //Import models here! 
+const { User, project } = require('../models')
 
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
   //Query database here
+try { 
+  const userData = await User.findAll({
+    catch: (err) => {
+      res.status(400).json(err)
+    }
+  })
+  const users = userData.map((user) => user.get({ plain: true }))
+  res.render('homepage', {
+    users,
+    logged_in: req.session.logged_in
+  })
+} catch (err) {
+
 
   res.render('homepage')
-})
+}})
 
 router.get('/recipes', (req, res) => {
   //Query database here
