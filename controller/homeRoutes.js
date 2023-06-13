3086177228bd1f172dde794256f2101c0ae9e9ce
jake <mobleyj2@gmail.com>
2023-06-12 21:29:24 -0400

    add login screen and nav

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,9 +1,33 @@
 const router = require('express').Router()
+const session = require('express-session');
+const express =require('express')
+const app = express();
+
 //Import models here! 
+router.get('/login', (req, res) => {
+  //if user logged in render them to home routes if not render to handlebars
+  if (req.session.loggedIn) {
+res.render('homepage')
+  }
+    res.render('login')
+  })
+  app.use (session ({
+    secret:'yourSecertKey',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+app.get ('/', function(req, res) {
+  if (req.session.loggedIn){
+    res.send('welcome')
+  } else { 
+    res.redirect('/login')
+  }
+})
 
 router.get('/', (req, res) => {
   //Query database here
-
+console.log("homeroutes")
   res.render('homepage')
 })
 
