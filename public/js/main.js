fddb92d4457160220bbf5f72a1da57c2938c2563
David Girton <dgirton2@gmail.com>
2023-06-18 19:53:16 -0400

    edits on cookiesjs

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     cookies.forEach((cookie) => {
         const li = document.createElement('li');
-        li.innerHTML = `
-            <h3>${cookie.name}</h3>
-            <p>Ingredients: ${cookie.ingredients}</p>
-            <p>Instructions: ${cookie.instructions}</p>
-            <p>Difficulty: ${cookie.difficulty}</p>
-        `;
+        li.textContent = cookie.name;
         cookieList.appendChild(li);
     }
     );
